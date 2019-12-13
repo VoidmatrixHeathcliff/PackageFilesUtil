9c94efe5019be563b4853c3a603ea67f407cf88d
VoidmatrixHeathcliff <46210663+VoidmatrixHeathcliff@users.noreply.github.com>
2019-12-13 21:17:27 +0800

    Update FileOper.cpp

--- FileOper.cpp
+++ FileOper.cpp
@@ -1,105 +1,172 @@
-#include <iostream>
 #include <stdio.h>
-#include <iostream>
-#include "FileOper.h"
-#include <ctype.h>
+#include <string.h>
+#include <io.h>
+#include <atlstr.h>  
+#include <direct.h>
 
-using namespace std;
+#define BUFFSIZE 4096
+#define MAX_FNUM 50000
 
-string user_input;
-string password = "84265";
+struct _finddata_t files_info[MAX_FNUM];
+size_t file_num = 0;
 
-int main(void)
+char* direct_path;
+char  src_file_path[200];
+const char* src_files_path = "./data/*.*";
+const char* data_file_name = "PackageData.bin";
+const char* meta_data_name = "MetaData.bin";
+
+void init_path(void)
 {
-	cout << "[智能函数运算训练工具]\t版本号：v-19.12.12" << endl;
-	cout << "(c)2019 Cardinal Organization。保留所有权利。" << endl;
-	cout << "Powered By Voidmatrix\tData:2019.12" << endl;
-
-	cout << "\n请在下方输入所需计算的表达式，以 [;] 结尾：" << endl << "\n>>>";
-	cin >> user_input;
-	while (user_input != password)
+	if ((direct_path = _getcwd(NULL, 0)) != NULL)
 	{
-		cout << "\n输入格式错误，请尝试重新输入 ！" << endl << "\n>>>";
-		cin >> user_input;
+		memcpy(src_file_path, direct_path, sizeof(src_file_path));
+		CString cstr;
+		cstr.Format(src_file_path);
+		cstr.Replace("\\", "\\\\");
+		strcpy(src_file_path, cstr);
+	}
+}
+
+void get_all_files(void)
+{
+	init_path();
+	struct _finddata_t file;
+	intptr_t HANDLE = _findfirst(src_files_path, &file);
+	size_t temp_num = 0;
+	do
+	{
+		if (temp_num <= 2)
+		{
+			file_num = 0;
+		}
+		files_info[file_num] = file;
+		file_num++;
+		temp_num++;
+	} while (_findnext(HANDLE, &file) == 0);
+
+	_findclose(HANDLE);
+}
+
+void package_files(void)
+{
+	FILE* source;
+	FILE* dest;
+	size_t bytes;
+	static char temp[4096];
+
+	if ((dest = fopen(data_file_name, "wb+")) == NULL)
+	{
+		printf("Can't open data file : PackageData.bin !\n");
+		return;
 	}
 
-	cout << "\n通过验证 ！" << endl;
-	system("cls");
-
-	cout << "\t<<<    指令索引    >>>\t" << endl;
-	cout << "1.将PackageData.bin文件内容解包至./data/目录下；\n" << endl;
-	cout << "2.将./data/目录下所有文件打包至PackageData.bin文件；\n" << endl;
-	cout << "3.将./data/目录下所有文件删除；\n" << endl;
-	cout << "4.退出程序 [将自动清空./data/目录]\n" << endl;
-	cout << "9.获取全部指令介绍\n" << endl;
-	
-	while (user_input != "4")
+	for (size_t i = 0; i < file_num; i++)
 	{
-		cout << "\n>>>";
-		cin >> user_input;
-
-		if (user_input == "1")
+		char temp_path[200];
+		memcpy(temp_path, src_file_path, sizeof(src_file_path));
+		strcat(temp_path, "\\\\data\\\\");
+		char* path = strcat(temp_path, files_info[i].name);
+		if ((source = fopen(path, "rb")) == NULL)
 		{
-			cout << "\n请确认此操作会将./data/下现有内容覆盖，是否继续 ？\n\tY.继续\tN.取消" << endl << "\n";
-			string confirm_input;
-			cin >> confirm_input;
-			if (confirm_input == "Y" || confirm_input == "y")
-			{
-				read_files_info();
-				unpack_files();
-			}
-			else
-			{
-				continue;
-			}	
-		}
-		else if (user_input == "2")
-		{
-			cout << "\n请确认./data/目录下文件为所要进行打包文件，是否继续 ？\n\tY.继续\tN.取消" << endl << "\n";
-			string confirm_input;
-			cin >> confirm_input;
-			if (confirm_input == "Y" || confirm_input == "y")
-			{
-				get_all_files();
-				package_files();
-				write_files_info();
-			}
-			else
-			{
-				continue;
-			}	
-		}
-		else if (user_input == "3")
-		{
-			cout << "\n请确认./data/目录下所有文件都将被删除，是否继续 ？\n\tY.继续\tN.取消" << endl << "\n";
-			string confirm_input;
-			cin >> confirm_input;
-			if (confirm_input == "Y" || confirm_input == "y")
-			{
-				delete_files();
-			}
-			else
-			{
-				continue;
-			}
-		}
-		else if (user_input == "4")
-		{
-			delete_files();
-			break;
-		}
-		else if (user_input == "9")
-		{
-			cout << "\t<<<    指令索引    >>>\t" << endl;
-			cout << "1.将PackageData.bin文件内容解包至./data/目录下；\n" << endl;
-			cout << "2.将./data/目录下所有文件打包至PackageData.bin文件；\n" << endl;
-			cout << "3.将./data/目录下所有文件删除；\n" << endl;
-			cout << "4.退出程序 [将自动清空./data/目录]\n" << endl;
-			cout << "9.获取全部指令介绍\n" << endl;
+			printf("Can't open source file : %s !\n", files_info[i].name);
 		}
 		else
 		{
-			cout << "\n输入有误，尝试重新输入 [输入数字 '9' 获取全部指令介绍]" << endl;
-		}
+			while ((bytes = fread(temp, sizeof(char), BUFFSIZE, source)) > 0)
+			{
+				fwrite(temp, sizeof(char), bytes, dest);
+			}
+			printf("File %s\tpackaged SUCCESS !\n", files_info[i].name);
+			fclose(source);
+		}		
+	}		
+	fclose(dest);
+}
+
+void write_files_info(void)
+{
+	FILE* dest;
+	if ((dest = fopen(meta_data_name, "wb+")) == NULL)
+	{
+		printf("Can't open meta file : MetaData.bin !\n");
+		return;
 	}
+	fwrite(files_info, MAX_FNUM, sizeof(struct _finddata_t), dest);
+	fclose(dest);
+}
+
+void read_files_info(void)
+{
+	FILE* source;
+	if ((source = fopen(meta_data_name, "rb+")) == NULL)
+	{
+		printf("Can't open meta file : MetaData.bin !\n");
+		return;
+	}
+	fread(files_info, MAX_FNUM, sizeof(struct _finddata_t), source);
+	fclose(source);
+}
+
+void unpack_files(void)
+{
+	init_path();
+
+	FILE* source;
+	FILE* dest;
+	if ((source = fopen(data_file_name, "rb+")) == NULL)
+	{
+		printf("Can't open data file : PackageData.bin !\n");
+		return;
+	}
+
+	size_t index = 0;
+	while (strlen(files_info[index].name) != 0)
+	{
+		char temp_path[200];
+		memcpy(temp_path, src_file_path, sizeof(src_file_path));
+		strcat(temp_path, "\\\\data\\\\");
+		char* path = strcat(temp_path, files_info[index].name);
+
+		printf("%s\n", path);
+		if ((dest = fopen(path, "wb+")) == NULL)
+		{
+			printf("Can't open dest file : %s\t!\n", files_info[index].name);
+			index++;
+			continue;
+		}
+
+		char* buffer = (char*)malloc(files_info[index].size);
+		fread(buffer, sizeof(char), files_info[index].size / sizeof(char), source);
+		fwrite(buffer, sizeof(char), files_info[index].size / sizeof(char), dest);
+		fclose(dest);
+		free(buffer);
+		index++;
+	}
+	fclose(source);
+}
+
+void delete_files(void)
+{
+	if ((direct_path = _getcwd(NULL, 0)) != NULL)
+	{
+		memcpy(src_file_path, direct_path, sizeof(src_file_path));
+		CString cstr;
+		cstr.Format(src_file_path);
+		cstr.Replace("\\", "\\\\");
+		strcpy(src_file_path, cstr);
+	}
+	init_path();
+	char* current_path = _getcwd(NULL, 0);
+	char command[200] = "del \"";
+	if (current_path == NULL)
+	{
+		printf("Failed to get current directory !");
+		return;
+	}
+	strcat(command, current_path);
+	strcat(command, "\\data\\");
+	strcat(command, "*.*\" /f/s/q/a");
+	printf("%s\n", command);
+	system(command);
 }
